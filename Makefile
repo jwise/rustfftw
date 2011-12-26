86e88900a1555c12e53f8718add2135432dc15f4
Joshua Wise <joshua@joshuawise.com>
2011-12-26 03:11:08 -0500

    Convert to tests.

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
+check: fftw_test
+	./fftw_test
+
 fftw_test: libfftw.dylib fftw_test.rs
-	rustc -L . fftw_test.rs
+	rustc -L . fftw_test.rs --test
 
 # Not actually called 'libfftw.dylib' anymore.  Sigh.	
 libfftw.dylib: fftw.rc fftw.rs
--- fftw_test.rs
+++ fftw_test.rs
@@ -1,13 +1,26 @@
 use std;
 use fftw;
 
-fn main() {
-    let in = fftw::malloc_cpx64(32u);
-    let out = fftw::malloc_cpx64(32u);
+const epsilon : f64 = 0.01f64;
+
+pure fn approx(a: f64, b: f64) -> bool {
+    if ((a + epsilon) < b) { false }
+    else if ((a - epsilon) > b) { false }
+    else { true }
+}
+
+pure fn approx_c64(a: fftw::complex64, b: fftw::complex64) -> bool {
+    approx(a.r, b.r) && approx(a.i, b.i)
+}
+
+#[test]
+fn basic() {
+    let in = fftw::malloc_cpx64(16u);
+    let out = fftw::malloc_cpx64(16u);
     
-    let p = fftw::plan_dft_1d(32u, in, out, fftw::FORWARD, fftw::MEASURE);
+    let p = fftw::plan_dft_1d(16u, in, out, fftw::FORWARD, fftw::MEASURE);
     
-    uint::range(0u, 32u) { |i|
+    uint::range(0u, 16u) { |i|
         /* Should be in[i] = {r: 0.0f64, i: 0.0f64} ... */
         std::c_vec::set(in, i, {r: 0.0f64, i: 0.0f64});
         std::c_vec::set(out, i, {r: 0.0f64, i: 0.0f64});
@@ -17,7 +30,8 @@ fn main() {
 
     fftw::execute(p);
     
-    uint::range(0u, 32u) { |i|
-        log(error, (i, std::c_vec::get(in, i), std::c_vec::get(out, i)));
-    }
+    assert approx_c64(std::c_vec::get(out, 0u), {r: 1.0f64, i: 0.0f64});
+    assert approx_c64(std::c_vec::get(out, 1u), {r: 0.707f64, i: -0.707f64});
+    assert approx_c64(std::c_vec::get(out, 2u), {r: 0.0f64, i: -1.0f64});
+    assert approx_c64(std::c_vec::get(out, 3u), {r: -0.707f64, i: -0.707f64});
 }
